4729c76b08aa9dbaaae877615c03953d45ef6cbb
Christian1Binder <96103048+Christian1Binder@users.noreply.github.com>
2026-09-14 21:01:49 +0200

    Switch application to new founding workspace

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './styles.css'
+import AppV2 from './AppV2.jsx'
+import './v2.css'
 import './production.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AppV2 />
   </React.StrictMode>,
 )
